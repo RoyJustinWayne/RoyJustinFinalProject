4384899d64ea381e2244886ed46749938f8885d3
RoyJustinWayne <40181612+RoyJustinWayne@users.noreply.github.com>
2018-06-11 21:42:01 -0400

    Add files via upload
    Updated read SQL file for executing the commands. Works as a buffered reader. 
    
    Added DROP to initialization of the DB to make sure it's clean when creating new tables.

--- RoyJustinAssignment1Part0/Source/Src/ConfManagement.java
+++ RoyJustinAssignment1Part0/Source/Src/ConfManagement.java
@@ -1,6 +1,6 @@
 package Src;
 
-import java.io.IOException;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -15,6 +15,7 @@ public class ConfManagement extends HttpServlet
 {
 
 	private static final long serialVersionUID = 1L;
+	
 	public void main (String args[]) throws ClassNotFoundException 
 	{		
 	}
@@ -26,8 +27,9 @@ public class ConfManagement extends HttpServlet
 		myClass.post();
 	 }
 	
-	public void post()
+	public void post() throws IOException
 	{
+		
 	final String JDBC_DRIVER= "com.mysql.jdbc.Driver";
 	final String DB_URL="jdbc:mysql://localhost/sampledb";
 	final String User="john";
@@ -35,35 +37,42 @@ public class ConfManagement extends HttpServlet
 	Connection conn=null;
 	Statement stmt=null;
 	
-	try
-		{
-		Class.forName(JDBC_DRIVER);
-	
-		System.out.println("Attempting to connect: ");
-		conn = DriverManager.getConnection(DB_URL,User, Pass);
-	
-		System.out.println("Connection Successful");
-	
-		stmt=conn.createStatement();
-		//Fix this to read the SQL file and execute like this.
-		
-		//String sql="create table registration"+"(id INTEGER not null,"+"first VARCHAR(255),"+"last VARCHAR(255),"
-		//+"age INTEGER,"+"PRIMARY KEY(I'd))";
-	
-		//stmt.executeUpdate(sql);
-	
-		System.out.println("creating table in dB");
+	String s = new String();
+    StringBuffer sb = new StringBuffer();
+        try
+        {
+            FileReader fr = new FileReader(new File("user.sql"));
+ 
+            BufferedReader br = new BufferedReader(fr);
+ 
+            while((s = br.readLine()) != null)
+            {
+                sb.append(s);
+            }
+            br.close();
+
+            String[] arr = sb.toString().split(";");
+ 
+            Class.forName(JDBC_DRIVER);
+    		conn = DriverManager.getConnection(DB_URL,User, Pass);
+            Statement st =  conn.createStatement();
+ 
+            for(int i = 0; i<arr.length; i++)
+            {
+                if(!arr[i].trim().equals(""))
+                {
+                    st.executeUpdate(arr[i]);
+                    System.out.println(">>"+arr[i]);
+                }
+            }
+		System.out.println("Created Tables");
 		}
 	
 		catch(SQLException se)
-	
-		{} // Do nothing
+		{}
 	 	catch (ClassNotFoundException e1) 
 		{
 			e1.printStackTrace();
-		}
-		try
-		{
 		}
 			finally
 			{
@@ -71,7 +80,6 @@ public class ConfManagement extends HttpServlet
 				{
 					if(stmt!=null) conn.close();
 				}
-				
 				catch(SQLException se)
 				{ 
 					se.printStackTrace();
